44f3080811ebdc8c21ba671d92f57d45f5b15f41
Tianyi Liu <mrbaoziii@gmail.com>
2020-04-13 19:27:17 +0800

    update analyze

--- cfgs.py
+++ cfgs.py
@@ -40,7 +40,7 @@ LOG_PATH = os.path.join(RESULTS_PATH, "logs")
 # For Benchmarking
 DR_METHOD = ["TSNE", "PCA"]
 
-DR_DIM = [32, 16, 8, 2]
+DR_DIM = [64, 32, 16, 8, 4, 2]
 
 BCM_PATH = os.path.join(RESULTS_PATH, "benchmark")
 
